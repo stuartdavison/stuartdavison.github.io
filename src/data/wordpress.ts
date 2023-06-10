b60b61771eb63d67d3b145d887fac09c6cf4ba8d
Stuart Davison <85502362+stuartdavison@users.noreply.github.com>
2023-06-10 15:49:43 +0100

    Full Deploy

---
+++ src/data/wordpress.ts
@@ -0,0 +1,29 @@
+interface WPGraphQLParams {
+    query: string;
+    variables?: object;
+}
+
+export async function wpquery({ query, variables = {} }: WPGraphQLParams) {
+    const res = await fetch(
+        "https://headless.builtbygeeks.co.uk/blog/graphql",
+        {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                query,
+                variables,
+            }),
+        }
+    );
+
+    if (!res.ok) {
+        console.error(res);
+        return {};
+    }
+
+    const { data } = await res.json();
+
+    return data;
+}
